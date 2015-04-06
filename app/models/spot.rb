d86c9b4db6fd510e2fa1720034d22da99e2ef711
Janik Knittle <janik.knittle@gmail.com>
2015-04-05 18:28:37 -0600

    Update paperclip upload settings for thumb previews

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,6 +2,8 @@ class Spot < ActiveRecord::Base
 	acts_as_votable
 	belongs_to :user
 	has_many :comments
-	has_attached_file :image, styles: {large: "1024x768>", medium: "300x300>"}
+	has_attached_file :image, styles: {thumb: "400x400>", large: "1024x768>"}, :convert_options => {
+    :thumb => "-quality 95 -strip -unsharp 1.5x1+0.7+0.02" }
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
+
